af0f3d16a25fbfbd256da8e10174449a3eabaee6
Cristian Poll <cristian@cristianpoll.com>
2026-01-24 14:38:13 -0500

    add guesses

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,18 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess a number");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Secret number: {secret_number}");
+
     println!("Input a number:");
 
     let mut guess = String::new();
 
     io::stdin()
-        .read_line(&mut guess)
+        .read_line(&mut guess) //read_line appends stdin onto the specified string
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
